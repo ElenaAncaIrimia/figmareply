528e0923ff2d8c557d9327b8ef56d5e27dd207a4
ElenaAncaIrimia <anca.irimia244@gmail.com>
2024-03-15 02:56:26 +0100

    gris and responsive

--- FE-CAT-projecte-1/tailwind.config.js
+++ FE-CAT-projecte-1/tailwind.config.js
@@ -2,8 +2,14 @@ module.exports = {
   content: ["./index.html", "./input.css", "./formulario.html"],
   theme: {
     extend: {
-      boxShadow: {
+      screens: {
+        'xs': '376px', // Cambia el punto de quiebre para dispositivos móviles
+        'sm': '476px', // Cambia el punto de quiebre para dispositivos móviles
+        'md': '768px', // Cambia el punto de quiebre para tabletas
+        'lg': '1024px', // Cambia el punto de quiebre para pantallas de escritorio
+        'xl': '1280px', // Agrega un nuevo punto de quiebre
       }
+     
     }
     
     
